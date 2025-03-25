b71ec933026e35b94fc98c8a3b7738275bdb520a
abdoulayeleDABO <abdoulayeleom10@gmail.com>
2025-03-25 20:20:47 +0000

    Creation des fichier necessaire pour une bonne structure

---
+++ supermat.c
@@ -0,0 +1,8 @@
+/**
+  * Ecrire les implémentations des fonctions dont les prototypes sont déclarés dans supermat.h
+ */ 
+
+ // Implémentation de la fonction afficheMessage
+//  void afficheMessage(){
+//      printf("Hello World\n");
+//  }
---
+++ supermat.h
@@ -0,0 +1,21 @@
+#include "supermat.h"
+
+
+/** 
+  Ecrire ici les prototypes des fonctions de supermat.c
+ */
+
+// Exemple de fonction
+
+/**
+  * Fonction qui affiche un message
+  * mettre ici une description de la fonction
+  * Cette structure n'est pas obligatoire, mais elle permet de documenter le code
+  *
+  * @param void // aucun paramètre
+  * @return void // ne retourne rien
+  *
+*/
+
+// void afficheMessage();
+
